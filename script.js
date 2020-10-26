e088c0a426e0e674d947e585de120716744740f2
loulou <yourdj.france@gmail.com>
2020-10-26 15:57:00 +0100

    Simpson Challenge finished

---
+++ script.js
@@ -0,0 +1,21 @@
+function fetchSimpsonJSON() {
+    const url = 'https://simpsons-quotes-api.herokuapp.com/quotes';
+
+    axios.get(url)
+        .then(function (response) {
+            // let a = response.data
+            // console.log(a[0].character)
+            return response.data; 
+        })
+        .then(function (simpson) {
+            console.log('data decoded from JSON:', simpson);
+
+            // Build a block of HTML
+            const simpsonHtml = `
+                <p><strong>${simpson[0].character}</strong></p>
+                <img src="${simpson[0].image}" />`;
+            document.querySelector('#simpson').innerHTML = simpsonHtml;
+        });
+}
+
+fetchSimpsonJSON();
